22746f1a3fa7541f739d81d6be97e9f7bcda87ae
Dorota Niezborała <d.niezborala@merixstudio.com>
2017-06-12 21:06:06 +0200

    Initial commit

---
+++ todos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    texr = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
